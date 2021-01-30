98e4a5ec2cac06984635a03f02e234fbf226d5f5
Robin <eldritchnoises@gmail.com>
2021-01-30 11:29:21 -0500

    Blink

--- src/entities/player/AnimationController.gd
+++ src/entities/player/AnimationController.gd
@@ -4,9 +4,14 @@ extends AnimatedSprite
 var floortime := false
 var stop := false
 
+var blinkframe := 0
+
 func _process(delta):
 	if !get_parent().is_on_floor():
 		floortime = true
+		
+	if get_node_or_null("BlinkTimer"):
+		$Blink.frame = blinkframe
 
 func _on_Movement_movement_state_changed(previous, new):
 	if !stop:
@@ -33,9 +38,44 @@ func _on_Movement_movement_state_changed(previous, new):
 			floortime = false
 			if new == Movement.State.RUNNING:
 				frame = 1
+				
+		set_blink()
 
 func _on_Movement_facing_changed(previous, new):
 	if new == Global.HDirs.LEFT:
 		scale.x = -1
 	else:
 		scale.x = 1
+
+func _on_Animations_frame_changed():
+	set_blink()
+	
+func set_blink():
+	$Blink.visible = true
+	var off = 0
+	match animation:
+		"idle", "swim":
+			if frame == 0:
+				off = 0
+			else:
+				off = 1
+		"walk":
+			if frame == 0:
+				off = -1
+			else:
+				off = 0
+		"jump":
+			off = -1
+		"hurt":
+			$Blink.visible = false
+	$Blink.offset.y = off
+
+func _on_BlinkTimer_timeout():
+	match blinkframe:
+		0:
+			$BlinkTimer.start(0.1 + rand_range(0, 0.1))
+			blinkframe = 1
+		1:
+			$BlinkTimer.start(0.5 + rand_range(0, 8))
+			blinkframe = 0
+
