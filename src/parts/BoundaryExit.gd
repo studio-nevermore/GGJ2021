67159431dbd54bc13ee5f6c92d573e17a5c7d1ac
Robin <eldritchnoises@gmail.com>
2021-01-31 16:15:41 -0500

    Extra boundary exit

--- src/parts/BoundaryExit.gd
+++ src/parts/BoundaryExit.gd
@@ -2,6 +2,7 @@ class_name BoundaryExit
 extends Area2D
 
 export(String) var exit_room = ""
+export(String) var exit_room_glitched = ""
 export(Global.Dirs) var exit_direction = Global.Dirs.LEFT
 export(int) var entrance_index = 0
 
@@ -25,6 +26,8 @@ func _on_BoundaryExit_body_entered(body):
 	
 	Global.current_boundary_entrance = entrance_index
 
+	if Stats.game_data[Stats.Data.glitched]:
+		exit_room = exit_room_glitched
 	if exit_room != "":
 		Global.current_room_control.room_change(exit_room)
 	else:
